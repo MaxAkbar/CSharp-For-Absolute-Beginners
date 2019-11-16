c36647c1e82b75d91659735b301026958b998225
Max Akbar <maximum2000@hotmail.com>
2019-11-15 20:35:26 -0800

    Added new project to read and write to file.

---
+++ FileReadWrite/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace FileReadWrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string fileContent = File.ReadAllText("SimpleText.txt");
+
+            // 1. read csv file line by line
+            string[] fileContent = File.ReadAllLines("ProcessMe.csv");
+
+            // 2. search for model get the value
+            for (int i = 0; i < fileContent.Length; i++)
+            {
+                string line = fileContent[i];
+
+                if (line.Contains("Ford"))
+                {
+                    // 3. output the price
+                    string[] columns = line.Split(',');
+
+                    File.AppendAllText("processedFile.txt", columns[1] + ": " + columns[3]);
+                    //Console.WriteLine(columns[3]);
+                }
+            }
+
+            //Console.WriteLine(fileContent);
+        }
+    }
+}
