f3fb26eb4b9f9090f1be256a2fef287e4cd41711
Max Akbar <maximum2000@hotmail.com>
2019-11-18 20:00:44 -0800

    Adding new project types.

---
+++ FileHelper/MyFileHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace FileHelper
+{
+    public class MyFileHelper
+    {
+        public static string[] OpenFile(string filePath)
+        {
+            return File.ReadAllLines(filePath);
+        }
+
+        public static void AppendToFile(string filePath, string content)
+        {
+            File.AppendAllText(filePath, content);
+        }
+    }
+}
--- FileReadWrite/Program.cs
+++ FileReadWrite/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FileHelper;
+using System;
 using System.IO;
 
 namespace FileReadWrite
